b9d9d71dd700da03aaff4b6f400a4bda03fab9ec
RAJ ARYAN <patnashubham10@gmail.com>
2025-09-18 07:27:34 +0000

    Conversion data type

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,18 @@
+let score = "raj aryan"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = "ram"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 56
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
