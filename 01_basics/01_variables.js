064c3fa7708b4d72354d8557806089a02cfdc365
RAJ ARYAN <patnashubham10@gmail.com>
2025-09-18 07:07:58 +0000

    DataVariables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 135455
+let accountEmail = "raj@gmail.com"
+var accountPassword = "77777"
+accountCity = "hyd"
+let accountState;
+//accountId = 2 // not allowed
+
+accountEmail = "rraj@gmail.com"
+accountPassword = "565676"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+prefer not use var
+issue in block scope and functional scope
+*/
