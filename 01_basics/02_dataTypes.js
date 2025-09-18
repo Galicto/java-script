0eb96f74274c41a95f35efdb68402920e9945a30
RAJ ARYAN <patnashubham10@gmail.com>
2025-09-18 07:05:36 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all code as newer version
+
+//alert(3+3)//we are using node js not browser
+
+console.log(3+3);
+console.log("Raj Aryan");
+//code readablity is a must
+//java standards on tc.39.es
+
+
+let name = "raj aryan"
+let age = 20
+let isLoggedIn = true
+
+//number =>2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => stand alone vale
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);
