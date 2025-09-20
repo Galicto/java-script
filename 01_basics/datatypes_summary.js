a597f2e8dad412a224ce4b667c9d7cdaed574657
RAJ ARYAN <patnashubham10@gmail.com>
2025-09-20 10:51:02 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// primitive
+
+// 7 categories : string number boolearn null undefined symbol bigint 
+
+// refrence type (non primitive)
+
+// array objects functions
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+
+//const bigNumber = 6258677856756785675n
+
+const heros = ["shaktiman","naagraj","dogesh"]
+
+
+let myObj = {
+    name: "raj",
+    age : 20,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof anotherId);
