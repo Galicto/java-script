6d2b2e15266fb20f22a60ba4879e7c48450ff21b
RAJ ARYAN <patnashubham10@gmail.com>
2025-09-21 10:53:41 +0000

    arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+//array
+
+const myArr = [1,2,3,4,6,5]
+
+//console.log(myArr[0]);
+
+//array methods
+
+// myArr.push(6)
+// console.log(myArr);
+//myArr.unshift(88)
+
+myArr.shift()
+
+
+console.log(myArr);
+
+// slice ,splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.splice(1,3)
+
+console.log(myn1);
+console.log("B",myArr);
+
+//slice doesnt affect the array and its indistructive
+//splice affect the orignal array and it makes changes in the orignal array and its distructive
+
+
+
+
+
