61070947ca6af397b7f13f11f96825be440ea69b
RAJ ARYAN <patnashubham10@gmail.com>
2025-09-23 09:35:06 +0000

    objects final

---
+++ 05_objects.js
@@ -0,0 +1,25 @@
+       const course = {
+        courseName: "JavaScript",
+        price: 999,
+        courseInstructor: "hitesh"
+}
+const{courseInstructor} = course
+console.log(courseInstructor);
+
+// const navbar = (props.company) {
+
+// }
+// navbar(company: "hitesh")
+
+// {
+//     "name": "hitesh",
+//     "course": "javascript",
+//     "price":"free",
+//     courseInstructor: "hitesh"
+// }
+
+[
+    {},
+    {},
+    {},
+]
