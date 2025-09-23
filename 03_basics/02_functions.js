7a968cda8aa57f12c83f3c5536b07240a545c8f0
RAJ ARYAN <patnashubham10@gmail.com>
2025-09-23 10:15:25 +0000

    functions secound part

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,22 @@
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(100,200,300,400))
+
+const user = {
+    userName: "hitesh",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`user name is ${anyobject.userName} and price is ${anyobject.price}`);
+}
+
+const myNewArray = [1,2,3,4,5,6,7,8,9]
+
+function returnSecoundValue(getArray){
+    return getArray[4]
+}
+
+console.log(returnSecoundValue(myNewArray));
